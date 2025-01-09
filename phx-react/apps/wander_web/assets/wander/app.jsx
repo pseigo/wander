0324bf702c310faadb692f206b97acf85faa49e9
Peyton Seigo <pseigo@yahoo.com>
2025-01-08 23:03:55 -0700

    [style] Prettier autoformat

--- phx-react/apps/wander_web/assets/wander/app.jsx
+++ phx-react/apps/wander_web/assets/wander/app.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
-	return <h1>Hello, world!</h1>;
+  return <h1>Hello, world!</h1>;
 }
 
 function init() {
-	// Clear the existing HTML content
-	document.body.innerHTML = '<div id="app"></div>';
+  // Clear the existing HTML content
+  document.body.innerHTML = '<div id="app"></div>';
 
-	// Render your React component instead
-	const root = createRoot(document.getElementById("app"));
-	root.render(
+  // Render your React component instead
+  const root = createRoot(document.getElementById("app"));
+  root.render(
     <StrictMode>
       <App />
     </StrictMode>
@@ -20,4 +20,3 @@ function init() {
 
 // Uncomment this line to replace every page with the React example.
 init();
-
