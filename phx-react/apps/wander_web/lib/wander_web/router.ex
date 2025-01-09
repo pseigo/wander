15522d64aa6ba64e7a50ee67f15bd72847e4d9ee
Peyton Seigo <pseigo@yahoo.com>
2025-01-09 00:35:07 -0700

    [gw] Route everything to React

--- phx-react/apps/wander_web/lib/wander_web/router.ex
+++ phx-react/apps/wander_web/lib/wander_web/router.ex
@@ -18,6 +18,7 @@ defmodule WanderWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/*rest", PageController, :home
   end
 
   # Other scopes may use custom stacks.
