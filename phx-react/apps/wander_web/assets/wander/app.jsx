7876f94f6f5adc17ffb9d4b26faee27688590b9b
Peyton Seigo <pseigo@yahoo.com>
2025-01-09 01:54:22 -0700

    [style] Prettier autoformat

--- phx-react/apps/wander_web/assets/wander/app.jsx
+++ phx-react/apps/wander_web/assets/wander/app.jsx
@@ -6,36 +6,40 @@ import { Route, useLocation } from "wouter";
 function App() {
   const [location, navigate] = useLocation();
 
-  return <div>
-    <h1>Hello, world!</h1>
+  return (
+    <div>
+      <h1>Hello, world!</h1>
 
-    <p>The location is <code>{location}</code>.</p>
+      <p>
+        The location is <code>{location}</code>.
+      </p>
 
-    <button
-      className={clsx([
-        "px-touch/2 min-h-touch",
-        "rounded",
+      <button
+        className={clsx([
+          "px-touch/2 min-h-touch",
+          "rounded",
 
-        "hover:shadow active:shadow-inner",
+          "hover:shadow active:shadow-inner",
 
-        "text-white",
-        "bg-sky-500 hover:bg-sky-400 active:bg-sky-600",
-        "border border-sky-600 hover:border-sky-500 active:border-sky-600",
-        "transition-colors duration-100"
-      ])}
-      onClick={() => navigate("/map")}
-    >
-      Start Mapping!
-    </button>
+          "text-white",
+          "bg-sky-500 hover:bg-sky-400 active:bg-sky-600",
+          "border border-sky-600 hover:border-sky-500 active:border-sky-600",
+          "transition-colors duration-100",
+        ])}
+        onClick={() => navigate("/map")}
+      >
+        Start Mapping!
+      </button>
 
-    <Route path="/">
-      <p>{"Home."}</p>
-    </Route>
+      <Route path="/">
+        <p>Home.</p>
+      </Route>
 
-    <Route path="/map">
-      <p>{"I'm a map!"}</p>
-    </Route>
-  </div>;
+      <Route path="/map">
+        <p>{"I'm a map!"}</p>
+      </Route>
+    </div>
+  );
 }
 
 function init() {
