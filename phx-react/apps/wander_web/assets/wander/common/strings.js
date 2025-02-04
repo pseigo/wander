63af73d55b0f5b583f386d247ce0e03396751122
Peyton Seigo <pseigo@yahoo.com>
2025-02-03 18:22:44 -0700

    [gw] Add 'capitalize' string util

--- phx-react/apps/wander_web/assets/wander/common/strings.js
+++ phx-react/apps/wander_web/assets/wander/common/strings.js
@@ -14,6 +14,26 @@
  * limitations under the License.
  */
 
+// TODO: unit test the functions in this module
+
+/**
+ * Returns a copy of `str` with the first character uppercased.
+ *
+ * @example
+ * ```js
+ * import { capitalize } from "/wander/common/strings";
+ * console.log(capitalize("hello")); //=> "Hello"
+ * console.log(capitalize(""));      //=> ""
+ * ```
+ *
+ * @param {string} str
+ *
+ * @returns {string}
+ */
+export function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export function randomLowerAlphaNumericString() {
   return Math.random().toString(36).slice(2);
 }
